628baa6d9f466c12d6de035c09f03ecb77436436
Sebastian Lacoste <101532709+sebastianlacoste@users.noreply.github.com>
2022-06-22 15:38:16 -0300

    Add filters and table to display

--- src/pages/CryptoData.jsx
+++ src/pages/CryptoData.jsx
@@ -1,8 +1,12 @@
+import Filter from "../components/aside/Filter";
+import CryptoTable from "../components/main/CryptoTable";
+
 const CryptoData = () => {
 	return (
 		<>
-			<main className="flex justify-center items-center">
-				<h1 className="text-6xl text-tracrypt-wt">CryptoData</h1>
+			<main className="flex gap-10 h-full">
+				<Filter />
+				<CryptoTable />
 			</main>
 		</>
 	);
