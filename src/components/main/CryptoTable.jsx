6221192e887cf76a1a4f33e3a7b45a24d3372dfc
Sebastian Lacoste <101532709+sebastianlacoste@users.noreply.github.com>
2022-06-22 16:50:02 -0300

    Add table head

--- src/components/main/CryptoTable.jsx
+++ src/components/main/CryptoTable.jsx
@@ -1,11 +1,23 @@
 const CryptoTable = () => {
-  return (
-    <div className="w-11/12 text-tracrypt-wt bg-tracrypt-dk rounded-tl-lg shadow-2xl shadow-black max-h-full">
-        <div className="">
-            <h1>CryptoTable</h1>
-        </div>
-    </div>
-  )
-}
+	const tableHeader = ["#", "Coin", "Price", "1h", "24h", "7d", "24h Vol.", "Mkt. Cap"];
+	const thList = tableHeader.map((header) => <th>{header}</th>);
 
-export default CryptoTable
+	return (
+		<div className="w-11/12 text-tracrypt-wt bg-tracrypt-dk rounded-tl-lg shadow-2xl shadow-black max-h-full">
+			<div className="">
+				<table className="w-full">
+					<thead>
+						<tr className="flex justify-around items-center bg-tracrypt-dk-f py-3 text-center border-b-2 border-b-tracrypt-bl shadow-lg shadow-black cursor-default">
+							{thList}
+						</tr>
+					</thead>
+					<tbody>
+						<tr></tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
+};
+
+export default CryptoTable;
