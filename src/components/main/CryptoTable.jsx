60061bf7ea54db386d4dc6c3518530b7c643be15
Sebastian Lacoste <101532709+sebastianlacoste@users.noreply.github.com>
2022-06-22 15:34:28 -0300

    Add table to display currencies data

---
+++ src/components/main/CryptoTable.jsx
@@ -0,0 +1,12 @@
+const CryptoTable = () => {
+    return (
+      <div className="w-11/12 text-tracrypt-wt bg-tracrypt-dk rounded-tl-lg 
+      shadow-2xl shadow-black">
+          <div className="">
+              <h1>CryptoTable</h1>
+          </div>
+      </div>
+    )
+  }
+  
+  export default CryptoTable
