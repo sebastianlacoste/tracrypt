e55f5578da26363595dfb0a203d8ef919149ba86
Sebastian Lacoste <101532709+sebastianlacoste@users.noreply.github.com>
2022-06-22 16:43:59 -0300

    Change distribution method for the aside menu

--- src/components/aside/Filter.jsx
+++ src/components/aside/Filter.jsx
@@ -2,27 +2,25 @@ import FilterOption from "./FilterOption";
 
 const Filter = () => {
 	return (
-		<div className="w-2/12 text-tracrypt-wt font-bold tracking-widest text-xl bg-tracrypt-dk rounded-tr-xl shadow-2xl shadow-black text-center flex flex-col">
+		<div className="w-2/12 text-tracrypt-wt font-extralight tracking-widest text-xl bg-tracrypt-dk rounded-tr-xl shadow-2xl shadow-black text-center flex flex-col justify-between max-h-full overflow-x-hidden">
 			<div className="w-full z-10">
 				<div className="bg-tracrypt-dk-f py-3 text-center border-b-2 border-b-tracrypt-bl shadow-lg shadow-black cursor-default">
 					<h1 className="uppercase">Filters</h1>
 				</div>
 			</div>
 
-			<FilterOption icon="fiat" name="Fiat Currencies" />
+			<FilterOption icon="fiat" name="Currencies" />
 
-			<FilterOption icon="marketCap" name="Market Cap" />
+			<FilterOption icon="marketCap" name="Mkt Cap" />
 
 			<FilterOption icon="volumen" name="Volumen" />
 
 			<FilterOption icon="gainers" name="Gainers" />
 
-			<FilterOption icon="losers" name="Losers" />
-
 			{/* Disclaimer */}
-			<div className="w-full z-0 hover:cursor-default">
+			<div className="w-full z-0 shadow-lg shadow-black hover:cursor-default">
 				<div className="flex justify-center bg-tracrypt-dk-f py-3 text-center border-b-2 border-b-red-500 shadow-md shadow-black hover:bg-red-900 transition-all">
-					<div className="flex items-center gap-4">
+					<div className="flex items-center gap-2">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							width="22"
@@ -38,32 +36,29 @@ const Filter = () => {
 						<h1 className="text-red-500">Disclaimer</h1>
 					</div>
 				</div>
-			</div>
-			<div className="w-full">
-				<div className="py-2 bg-tracrypt-gr-dk shadow-lg shadow-black">
-					<p className="text-[10px] px-1 leading-normal font-bold">
-						No part of the content that we provide constitutes financial advice,
-						legal advice or any other form of advice meant for your specific
-						reliance for any purpose. Any use or reliance on our content is
-						solely at your own risk and discretion.
-					</p>
+				<div className="w-full">
+					<div className="py-2 bg-tracrypt-gr-dk">
+						<p className="text-[10px] px-1 leading-normal font-bold overflow-hidden">
+							No part of the content that we provide constitutes financial
+							advice, legal advice or any other form of advice meant for your
+							specific reliance for any purpose. Any use or reliance on our
+							content is solely at your own risk and discretion.
+						</p>
+					</div>
 				</div>
 			</div>
 
 			{/* Coingecko Atribbute */}
-			<div className="w-full z-0 hover:cursor-default">
+			<div className="w-full z-0 hover:cursor-default mb-4">
 				<div className="p-3 flex flex-col justify-center items-center">
 					<a
-						className="container py-1 rounded-lg shadow-md shadow-tracrypt-wt hover:shadow-lg hover:shadow-tracrypt-wt transition-all"
+						className="container py-1 rounded-lg shadow-md shadow-green-500 hover:shadow-lg hover:shadow-green-500 transition-all"
 						href="https://www.coingecko.com/en/api"
 						target="_blank"
-						rel="noopener noreferrer"
+						rel="noopener nofollow"
 					>
 						<h1 className="text-sm font-extralight">Powered by</h1>
-
-						<h1 className="text-xl text-green-500 font-extralight">
-							Coingecko API
-						</h1>
+						<h1 className="text-xl text-green-500">Coingecko API</h1>
 					</a>
 				</div>
 			</div>
