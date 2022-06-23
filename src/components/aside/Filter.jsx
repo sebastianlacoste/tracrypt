6a0bca4195a6b36e78e9232511223f93c9da78cd
Sebastian Lacoste <101532709+sebastianlacoste@users.noreply.github.com>
2022-06-22 21:37:57 -0300

    Change aside menu disclaimer hover effect

--- src/components/aside/Filter.jsx
+++ src/components/aside/Filter.jsx
@@ -18,8 +18,8 @@ const Filter = () => {
 			<FilterOption icon="gainers" name="Gainers" />
 
 			{/* Disclaimer */}
-			<div className="w-full z-0 shadow-lg shadow-black hover:cursor-default">
-				<div className="flex justify-center bg-tracrypt-dk-f py-3 text-center border-b-2 border-b-red-500 shadow-md shadow-black hover:bg-red-900 transition-all">
+			<div className="w-full z-0 shadow-lg shadow-black text-red-500 hover:cursor-default hover:bg-red-900 hover:text-tracrypt-wt transition-all">
+				<div className="flex justify-center py-3 text-center border-b-2 border-b-red-500 shadow-md shadow-black ">
 					<div className="flex items-center gap-2">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
@@ -33,12 +33,12 @@ const Filter = () => {
 								d="M21.4 18.063L11.65 1.188A.741.741 0 0011 .813a.739.739 0 00-.65.375L.6 18.063a.75.75 0 00.65 1.125h19.5a.75.75 0 00.65-1.125zM10.25 7.75c0-.103.084-.188.188-.188h1.124c.104 0 .188.085.188.188v4.313a.188.188 0 01-.188.187h-1.124a.188.188 0 01-.188-.188V7.75zM11 16a1.125 1.125 0 010-2.25A1.125 1.125 0 0111 16z"
 							></path>
 						</svg>
-						<h1 className="text-red-500">Disclaimer</h1>
+						<h1 className="">Disclaimer</h1>
 					</div>
 				</div>
 				<div className="w-full">
 					<div className="py-2 bg-tracrypt-gr-dk">
-						<p className="text-[10px] px-1 leading-normal font-bold overflow-hidden">
+						<p className="text-[10px] px-1 leading-normal font-bold overflow-hidden text-tracrypt-wt">
 							No part of the content that we provide constitutes financial
 							advice, legal advice or any other form of advice meant for your
 							specific reliance for any purpose. Any use or reliance on our
