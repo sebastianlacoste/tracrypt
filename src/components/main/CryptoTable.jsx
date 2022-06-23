e3bc0973fc1f7167af62dbb87d564aad4d0546b9
Sebastian Lacoste <101532709+sebastianlacoste@users.noreply.github.com>
2022-06-23 10:59:57 -0300

    Add table body row generator and apply basic filters to table data

--- src/components/main/CryptoTable.jsx
+++ src/components/main/CryptoTable.jsx
@@ -1,6 +1,31 @@
-import CoinData from "./CoinData";
+import { useState, useEffect } from "react";
+
+import useFilterSelected from "../../hooks/useFilterSelected";
+
+import coinData from "../../helpers/CoinData";
+import idGenerate from "../../helpers/IdGenerate";
+import { volumenSort, mktCapSort } from "../../helpers/OrderSort";
 
 const CryptoTable = () => {
+	const { currencieChoose, setCurrencieChoose, orderChoose, setOrderChoose } =
+		useFilterSelected();
+	const [coinRender, setCoinRender] = useState([]);
+
+	useEffect(() => {
+		const getApiData = async (currency) => {
+			if (currencieChoose === "") {
+				setCurrencieChoose("usd");
+			}
+
+			if (orderChoose === "") {
+				setOrderChoose("Mkt Cap");
+			}
+
+			setCoinRender(await coinData(currency));
+		};
+		getApiData(currencieChoose);
+	}, []);
+
 	const tableHeader = [
 		"#",
 		"Coin",
@@ -13,10 +38,65 @@ const CryptoTable = () => {
 	];
 
 	const thList = tableHeader.map((header) => (
-		<th className="w-full py-3 hover:bg-tracrypt-gr-dk transition-all font-extralight tracking-widest text-xl ">
+		<th
+			className="w-full py-3 hover:bg-tracrypt-gr-dk transition-all font-extralight tracking-widest text-xl"
+			key={`${idGenerate()}`}
+		>
 			{header}
 		</th>
 	));
+
+	if (orderChoose === "Volumen") {
+		coinRender.sort(volumenSort);
+	} else if (orderChoose === "Mkt Cap") {
+		coinRender.sort(mktCapSort);
+	}
+
+	let colorChange = false;
+
+	const trList = coinRender.map((data) => {
+		const dataArray = Object.values(data);
+
+		let tdCounter = 0;
+
+		let tdListGen = dataArray.map((coinInfo) => {
+			if (tdCounter > 2 && tdCounter < 6) {
+				if (coinInfo > 0) {
+					tdCounter++;
+					return (
+						<td className="w-full p-3 text-green-400" key={idGenerate()}>
+							+{coinInfo}%
+						</td>
+					);
+				} else {
+					tdCounter++;
+					return (
+						<td className="w-full p-3 text-red-400" key={idGenerate()}>
+							{coinInfo}%
+						</td>
+					);
+				}
+			} else {
+				tdCounter++;
+				return (
+					<td className="w-full p-3" key={idGenerate()}>
+						{coinInfo}
+					</td>
+				);
+			}
+		});
+
+		return (
+			<tr
+				className={`${
+					colorChange ? "bg-tracrypt-gr-dk" : "bg-tracrypt-dk"
+				} ${(colorChange =
+					!colorChange)} py-2 pr-8 flex justify-evenly items-center transition-all text-center hover:shadow-md hover:shadow-tracrypt-bl hover:scale-105 ease-out duration-300 cursor-default`}
+			>
+				{tdListGen}
+			</tr>
+		);
+	});
 
 	return (
 		<div
@@ -30,9 +110,7 @@ const CryptoTable = () => {
 							{thList}
 						</tr>
 					</thead>
-					<tbody>
-						<CoinData />
-					</tbody>
+					<tbody>{trList}</tbody>
 				</table>
 			</div>
 		</div>
@@ -40,3 +118,12 @@ const CryptoTable = () => {
 };
 
 export default CryptoTable;
+
+/*
+
+	useEffect(() => {
+		
+		
+	}, [orderChoose]);
+
+*/
