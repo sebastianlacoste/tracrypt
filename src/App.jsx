b8b345063d4a8f1e27b8bc00f1415355bd45822a
Sebastian Lacoste <101532709+sebastianlacoste@users.noreply.github.com>
2022-06-22 23:12:55 -0300

    Add context scope

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import AppLayout from "./layout/AppLayout";
 import CryptoData from "./pages/CryptoData";
 
+import { FilterSelected } from "./context/FilterSelected";
+
 function App() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<AppLayout />}>
-					<Route index element={<CryptoData />}/>
-				</Route>
-			</Routes>
+			<FilterSelected>
+				<Routes>
+					<Route path="/" element={<AppLayout />}>
+						<Route index element={<CryptoData />} />
+					</Route>
+				</Routes>
+			</FilterSelected>
 		</BrowserRouter>
 	);
 }
