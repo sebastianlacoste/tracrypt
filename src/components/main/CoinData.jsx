ea07a3ad29e06bb91a229819b8fe3e8eba404ca2
Sebastian Lacoste <101532709+sebastianlacoste@users.noreply.github.com>
2022-06-22 21:42:52 -0300

    Add striped effect to table rows

--- src/components/main/CoinData.jsx
+++ src/components/main/CoinData.jsx
@@ -1,6 +1,8 @@
 // ["#", "Coin", "Price", "1h", "24h", "7d", "24h Vol.", "Mkt. Cap"];
 
 const CoinData = () => {
+	let colorChange = false;
+
 	const tableBody = [
 		{
 			id: "1",
@@ -22,19 +24,47 @@ const CoinData = () => {
 			volume: "$24,118,373,349",
 			mktCap: "$556,801,193,885",
 		},
+		{
+			id: "3",
+			coin: "Monero XMR",
+			price: "$106.60",
+			hour1: "-1.1%",
+			hour24: "2.4%",
+			week: "5.4%",
+			volume: "$24,118,373,349",
+			mktCap: "$556,801,193,885",
+		},
+		{
+			id: "4",
+			coin: "Chainlink",
+			price: "$7.60",
+			hour1: "-1.1%",
+			hour24: "2.4%",
+			week: "5.4%",
+			volume: "$24,118,373,349",
+			mktCap: "$556,801,193,885",
+		},
 	];
 
+	// Obtenemos el Objeto del Array
 	const tdList = tableBody.map((data) => {
+		// Obtenemos los valores del Objeto en un Array
 		const dataArray = Object.values(data);
 
+		// Recorremos los valores obtenidos para generar los <td>
 		let tdListGen = dataArray.map((coinInfo) => (
-			<td className="w-full p-3 hover:bg-tracrypt-gr-dk transition-all">
-				{coinInfo}
-			</td>
+			<td className="w-full p-3">{coinInfo}</td>
 		));
 
 		return (
-			<tr className="flex justify-evenly items-center bg-tracrypt-dk-f text-center border-b-2 border-b-tracrypt-bl shadow-lg shadow-black cursor-default">
+			<tr
+				className={`${
+					colorChange
+						? "bg-tracrypt-gr-dk"
+						: "bg-tracrypt-dk"
+				} ${(colorChange =
+					!colorChange)} flex justify-evenly items-center transition-all text-center hover:shadow-md hover:shadow-tracrypt-bl hover:scale-105 cursor-default`}
+			>
 				{tdListGen}
 			</tr>
 		);
